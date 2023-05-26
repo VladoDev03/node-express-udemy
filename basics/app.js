aa8145c279141686ea2421ded0d94896b778d03d
Vladimir Stoyanov <vladsto101@gmail.com>
2023-05-27 02:26:52 +0300

    Started the course basic section.

---
+++ basics/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello, World!</h1>');
+    res.end();
+});
+
+server.listen(3000);
