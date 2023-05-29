6ba07c3ec931a31fdd7c8bbfef54b8dec354d970
Vladimir Stoyanov <vladsto101@gmail.com>
2023-05-29 19:55:55 +0300

    Finished express intro.

--- working-with-express/app.js
+++ working-with-express/app.js
@@ -1,5 +1,24 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer();
+const path = require('path');
 
-server.listen(3000);
+const express = require('express');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000);
