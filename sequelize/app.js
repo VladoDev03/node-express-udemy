31257d9bf42ec04c1af34f5e8f39b2e47a833841
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-11 15:16:48 +0200

    Created cart model and added back cart functionality.

--- sequelize/app.js
+++ sequelize/app.js
@@ -9,6 +9,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -38,6 +40,12 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.hasOne(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
     // .sync({ force: true })
     .sync()
@@ -53,8 +61,9 @@ sequelize
         return user;
     })
     .then(user => {
-        console.log(user);
-
+        return user.createCart();
+    })
+    .then(cart => {
         app.listen(3000);
     })
     .catch(err => {
--- sequelize/queries.sql
+++ sequelize/queries.sql
@@ -2,5 +2,7 @@ DROP TABLE products;
 
 SELECT * FROM products;
 SELECT * FROM users;
+SELECT * FROM carts;
+SELECT * FROM cartitems;
 
 DELETE FROM products WHERE id = 2;
