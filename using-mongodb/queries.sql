e29075ec49ec1c66115d25ef5f767c3e2c614f77
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-12 00:07:37 +0200

    Started mongodb module.

---
+++ using-mongodb/queries.sql
@@ -0,0 +1,10 @@
+DROP TABLE products;
+
+SELECT * FROM products;
+SELECT * FROM users;
+SELECT * FROM carts;
+SELECT * FROM cartitems;
+SELECT * FROM orders;
+SELECT * FROM orderitems;
+
+DELETE FROM products WHERE id = 2;
