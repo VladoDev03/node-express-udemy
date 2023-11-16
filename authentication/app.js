356fb1d130438d289dcc529d3589069bd47bb600
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-16 22:24:45 +0200

    Added csrf protection.

--- authentication/app.js
+++ authentication/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -13,10 +14,13 @@ const MONGODB_URI =
   'mongodb+srv://vladsto101:Salamur12@course.rgce3bw.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
+
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -27,6 +31,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(
   session({
     secret: 'my secret',
@@ -35,6 +40,8 @@ app.use(
     store: store
   })
 );
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -46,6 +53,13 @@ app.use((req, res, next) => {
       next();
     })
     .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+
+  next();
 });
 
 app.use('/admin', adminRoutes);
