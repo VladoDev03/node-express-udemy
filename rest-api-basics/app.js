dcec067249134c94146e6a3ad5300231e0b48ccc
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-21 21:32:04 +0200

    Added CORS.

--- rest-api-basics/app.js
+++ rest-api-basics/app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParses.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, PSOT, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-HEADERS', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
