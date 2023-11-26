2d4a4075a8513a5e7cffeee8b7024f35d80bbe89
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-27 00:02:00 +0200

    Improved socket.io initialization.

--- websockets-socketio/back-end/app.js
+++ websockets-socketio/back-end/app.js
@@ -65,12 +65,7 @@ mongoose.connect(
     .then(result => {
         const server = app.listen(8080);
 
-        const io = require('socket.io')(server, {
-            cors: {
-                origin: '*',
-                methods: ['GET', 'POST']
-            }
-        });
+        const io = require('./socket').init(server);
 
         io.on('connection', socket => {
             console.log('Client connected');
