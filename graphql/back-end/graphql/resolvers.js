2ccb177599e2497c8af891c805411748a5600eac
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-28 23:08:51 +0200

    Added input validation and error handling.

--- graphql/back-end/graphql/resolvers.js
+++ graphql/back-end/graphql/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
@@ -6,8 +7,29 @@ module.exports = {
     // createUser(args, req) {
     //     const email = args.userInput.email;
     // }
-    createUser: async function({ userInput }, req) {
-        const existingUser = await User.findOne({email: userInput.email});
+    createUser: async function ({ userInput }, req) {
+        const errors = [];
+
+        if (!validator.isEmail(userInput.email)) {
+            errors.push({ message: 'E-Mail is invalid.' });
+        }
+
+        if (
+            validator.isEmpty(userInput.password)
+            || !validator.isLength(userInput.password, { min: 5 })
+        ) {
+            errors.push({ message: 'Password is too short!' });
+        }
+
+        if (errors.length > 0) {
+            const error = new Error('Invalid input.');
+            error.data = errors;
+            error.code = 422;
+
+            throw error;
+        }
+
+        const existingUser = await User.findOne({ email: userInput.email });
 
         if (existingUser) {
             const error = new Error('User exists already!');
