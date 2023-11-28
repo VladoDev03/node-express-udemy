ebf60993967e5050c27effe1bc160fed207312cb
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-28 23:49:44 +0200

    Added user registration.

--- graphql/back-end/app.js
+++ graphql/back-end/app.js
@@ -42,6 +42,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
     next();
 });
 
@@ -49,7 +53,7 @@ app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
     graphiql: true,
-    formatError(err) {
+    customFormatErrorFn(err) {
         if (!err.originalError) {
             return err;
         }
