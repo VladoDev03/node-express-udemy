8193ce956e3dfdf07ec1c090adccd98e62829926
Vladimir Stoyanov <vladsto101@gmail.com>
2023-11-29 19:18:44 +0200

    Added user login.

--- graphql/back-end/graphql/schema.js
+++ graphql/back-end/graphql/schema.js
@@ -1,40 +1,52 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
+    mutation {
+      createUser(userInput:{email: "test@test.test", name: "Test", password: "tester123"}) {
+        _id
+        email
+      }
+    }
+    
     type Post {
-        _id: ID!,
-        title: String!,
-        content: String!,
-        imageUrl: String!,
-        creator: User!,
-        createdAt: String!,
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
         updatedAt: String!
     }
-
+    
     type User {
-        _id: ID!,
+        _id: ID!
         name: String!
         email: String!
-        password: String,
-        status: String,
+        password: String
+        status: String
         posts: [Post!]!
     }
-
+    
+    type AuthData {
+        token: String!
+        userId: String!
+    }
+    
     input UserInputData {
-        name: String,
-        email: String,
+        name: String
+        email: String
         password: String
     }
-
+    
     type RootQuery {
-        hello: String
+        login(email: String!, password: String!): AuthData!
     }
-
+    
     type RootMutation {
         # createUser(name: String, email: String, password: String)
         createUser(userInput: UserInputData): User!
     }
-
+    
     schema {
         query: RootQuery
         mutation: RootMutation
